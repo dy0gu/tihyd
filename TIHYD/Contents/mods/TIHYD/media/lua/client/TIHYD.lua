df93004f21e809cbcc2672e66c4a0142ed3f7eb4
Diogu-Simoes <dbsimoes20@gmail.com>
2023-09-05 01:21:15 +0100

    Format code

--- TIHYD/Contents/mods/TIHYD/media/lua/client/TIHYD.lua
+++ TIHYD/Contents/mods/TIHYD/media/lua/client/TIHYD.lua
@@ -12,9 +12,13 @@ function TIHYD.log(string)
 end
 
 function TIHYD.clamp(value, min, max)
-    if min and value < min then return min
-    elseif max and value > max then return max
-    else return value end
+    if min and value < min then
+        return min
+    elseif max and value > max then
+        return max
+    else
+        return value
+    end
 end
 
 function TIHYD.getSandboxValue(option, vartype)
@@ -93,7 +97,6 @@ function TIHYD.age(player, init)
 end
 
 function TIHYD.updateVisual(player, age)
-
     TIHYD.log("Grey hair is set to appear when player is " .. TIHYD.grayHairAge .. " years old.")
     TIHYD.log("White hair is set to appear when player is " .. TIHYD.whiteHairAge .. " years old.")
 
@@ -130,17 +133,18 @@ function TIHYD.tryDeath(player, age)
 
     local healthiness = 0
 
-    if(TIHYD.IsTraitsConsidered) then
+    if (TIHYD.IsTraitsConsidered) then
         healthiness = TIHYD.getHealthiness(player)
     end
 
-    local chance = TIHYD.clamp(((1 / (100 * math.exp(age * - 0.091))) - healthiness), 0, 100)
+    local chance = TIHYD.clamp(((1 / (100 * math.exp(age * -0.091))) - healthiness), 0, 100)
     local random = ZombRand(0, 10001) / 100
     local isDead = random < chance
 
     TIHYD.log("Player can die at his current age.")
     TIHYD.log("Player has a " .. math.floor(chance) .. "% chance of dying of old age today!")
-    TIHYD.log("Player rolled a " .. math.floor(random) .. "%, which is " .. (isDead and "less, so they died." or "more, so they lived."))
+    TIHYD.log("Player rolled a " ..
+        math.floor(random) .. "%, which is " .. (isDead and "less, so they died." or "more, so they lived."))
 
     if isDead then
         player:Kill(player)
@@ -156,14 +160,14 @@ function TIHYD.getHealthiness(player)
     ------------------------------------------------
 
     -- Traits that greatly lower the chance of death
-    local very_good_traits = {"Strong", "Athletic", "Resilient"}
+    local very_good_traits = { "Strong", "Athletic", "Resilient" }
     for trait in very_good_traits do
         if traits:contains(trait) then
             healthiness = healthiness + 5
         end
     end
     -- Traits that slightly lower the chance of death
-    local good_traits = {"Stout", "Fit", "Lucky"}
+    local good_traits = { "Stout", "Fit", "Lucky" }
     for trait in good_traits do
         if traits:contains(trait) then
             healthiness = healthiness + 2.5
@@ -175,14 +179,14 @@ function TIHYD.getHealthiness(player)
     ------------------------------------------------
 
     -- Traits that greatly raise the chance of death
-    local very_bad_traits = {"Obese", "VeryUnderweight", "Weak", "Unfit", "Smoker"}
+    local very_bad_traits = { "Obese", "VeryUnderweight", "Weak", "Unfit", "Smoker" }
     for trait in very_bad_traits do
         if traits:contains(trait) then
             healthiness = healthiness - 5
         end
     end
     -- Traits that slightly raise the chance of death
-    local bad_traits = {"ProneToIllness", "Feeble", "Unlucky", "OutOfShape", "Underweight", "Overweight"}
+    local bad_traits = { "ProneToIllness", "Feeble", "Unlucky", "OutOfShape", "Underweight", "Overweight" }
     for trait in bad_traits do
         if traits:contains(trait) then
             healthiness = healthiness - 2.5
